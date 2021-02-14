b18eb3b83e291638060673266ca0c3c29dfe6a16
Federico Gerardi <federico.gerardi03@gmail.com>
2021-02-14 15:25:05 +0100

    First Commit �

---
+++ kez.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Kez:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
+    def __init__(self, url):
+        self.url = url
+
+    def is_there_text_by_id(self, text, id):
+        """
+        Is there the text inside that id? True or False
+        """
+        request = requests.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(request.text, 'html.parser')
+
+        if text in str(soup.find(id=id)):
+            return True
+        else:
+            return False
+
+    def get_text_by_id(self, id):
+        """
+        Can I get the text inside that id? Sure :)
+        """
+        request = requests.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(request.text, 'html.parser')
+
+        return str(soup.find(id=id))
